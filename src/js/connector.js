72516443d032b1e697f9f627867122bf72f3221a
oibafs <fabioscaravelli@gmail.com>
2020-08-24 19:41:54 -0300

    change card-buttons declaration format

--- src/js/connector.js
+++ src/js/connector.js
@@ -100,15 +100,23 @@ const getWeatherBadges = (t, opts) =>
     return badges;
   });
 
+const displayCardButton = (t, options) => {
+  return [{
+    icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+    text: 'Postpone card',
+  }];
+}
+
 window.TrelloPowerUp.initialize(
   {
-    'card-buttons': function(t, options){
+/*    'card-buttons': function(t, options){
       return [{
         icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
         text: 'Postpone card',
       }];
-    },
-        'card-badges': getWeatherBadges,
+    },*/
+    'card-buttons': displayCardButton,
+    'card-badges': getWeatherBadges,
     'card-detail-badges': getWeatherBadges,
     'show-settings': t => {
       return t.popup({
