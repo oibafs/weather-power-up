71f1f7b57d81aa76592e0107bd4e215e38bbfbeb
oibafs <fabioscaravelli@gmail.com>
2020-08-24 19:28:07 -0300

    Add button

--- src/js/connector.js
+++ src/js/connector.js
@@ -102,7 +102,13 @@ const getWeatherBadges = (t, opts) =>
 
 window.TrelloPowerUp.initialize(
   {
-    'card-badges': getWeatherBadges,
+    'card-buttons': function(t, options){
+      return [{
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+        text: 'Postpone card',
+      }];
+    },
+        'card-badges': getWeatherBadges,
     'card-detail-badges': getWeatherBadges,
     'show-settings': t => {
       return t.popup({
